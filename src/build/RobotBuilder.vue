1a318708a2daab398e67628950b2a775e47c4a1e
Goloveshkin Nikolay <kolya-mp19@mail.ru>
2019-04-06 20:23:47 +0300

    round img

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,31 +2,31 @@
     <div>
         <div class="top-row">
             <div class="top part">
-                <img src="./images/head-big-eye.png" title="head"/>
-                <button class="prev-selector">&#9668;</button>
-                <button class="next-selector">&#9658;</button>
+                <img v-bind:src="availableParts.heads[selectedHeadIndex].src" title="head"/>
+                <button v-on:click="selectPreviosHead()" class="prev-selector">&#9668;</button>
+                <button v-on:click="selectNextHead()" class="next-selector">&#9658;</button>
             </div>
         </div>
         <div class="middle-row">
             <div class="left part">
-                <img src="./images/arm-articulated-claw.png" title="left arm"/>
+                <img v-bind:src="availableParts.arms[0].src" title="left arm"/>
                 <button class="prev-selector">&#9650;</button>
                 <button class="next-selector">&#9660;</button>
             </div>
             <div class="center part">
-                <img src="./images/torso-flexible-gauged.png" title="left arm"/>
+                <img v-bind:src="availableParts.torsos[0].src" title="left arm"/>
                 <button class="prev-selector">&#9668;</button>
                 <button class="next-selector">&#9658;</button>
             </div>
             <div class="right part">
-                <img src="./images/arm-dual-claw.png" title="left arm"/>
+                <img v-bind:src="availableParts.arms[0].src" title="left arm"/>
                 <button class="prev-selector">&#9650;</button>
                 <button class="next-selector">&#9660;</button>
             </div>
         </div>
         <div class="bottom-row">
             <div class="bottom part">
-                <img src="./images/base-single-wheel.png" title="left arm"/>
+                <img v-bind:src="availableParts.bases[0].src" title="left arm"/>
                 <button class="prev-selector">&#9668;</button>
                 <button class="next-selector">&#9658;</button>
             </div>
@@ -35,11 +35,34 @@
 </template>
 
 <script>
+import availableParts from "../data/parts";
+
+function getPreviousValidIndex(index, length) {
+  const deprecatedIndex = index -1;
+  return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
+}
+
+function getNextValidIndex(index, length) {
+  const incrementedIndex = index +1;
+  return incrementedIndex > length - 1 ? 0 : incrementedIndex;
+}
+
 export default {
     name: "RobotBuilder",
-    date() {
-      return {};
+    data() {
+      return {
+        availableParts,
+        selectedHeadIndex: 0,
+      };
     },
+    methods: {
+      selectNextHead() {
+        this.selectedHeadIndex +=1;
+      },
+      selectPreviosHead(){
+        this.selectedHeadIndex -=1;
+      },
+    }
 }
 </script>
 
