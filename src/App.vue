a19a043fb38b40d0f84d9b04cab5f1a6acd2476c
Goloveshkin Nikolay <kolya-mp19@mail.ru>
2019-06-12 18:35:19 +0300

    routing

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,30 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img src="./assets/build-a-bot-logo.png" alt="" class="logo">
-            Build-a-Bot
+            <!-- exact - switch для стилей -->
+            <router-link class="nav-link" :to="{name: 'Home'}" exact>
+              <img src="./assets/build-a-bot-logo.png" alt="" class="logo">
+              Build-a-Bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Build'}" exact>
+              Build
+            </router-link>
           </li>
         </ul>
       </nav>
     </header>
     <main>
-      <RobotBuilder />
+      <router-view/>
     </main>
   </div>
 </template>
 
 <script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'app',
-  components: {
-    RobotBuilder,
-  },
 };
 </script>
 
@@ -69,5 +72,13 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+/* класс для ссылкок по умолчанию */
+.router-link-active {
+  color: white;
 }
 </style>
